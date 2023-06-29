da8d205d35b67a9922e1b675304df0628c203ec7
unknown <melakualehegn34@gmail.com>
2023-06-29 03:01:26 +0300

    settimeout for error messages

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let submit = document.querySelector(".svg");
 submit.addEventListener('click', hundleClick)
 
 function hundleClick() {
-    let dayValue = document.querySelector("#day").value;
+    let dayValue = parseInt(document.querySelector("#day").value);
     let monthValue = parseInt(document.querySelector("#month").value);
     let yearValue = parseInt(document.querySelector("#year").value);
     let alertDay = document.querySelector(".alert-message-day");
@@ -11,13 +11,33 @@ function hundleClick() {
     let spanYear = document.querySelector(".span-year");
     let spanMonth = document.querySelector(".span-month");
     let spanDay = document.querySelector(".span-day");
-
-    if (dayValue === '') {
-        console.log("empty date")
+    let now = new Date()
+    let birthday = new Date(yearValue, monthValue - 1, dayValue)
+    if (isNaN(dayValue)) {
+        alertDay.textContent = "day must be valid day"
+        setTimeout(() => {
+            alertDay.textContent = "";
+        }, 4000);
+    }
+    else if (isNaN(monthValue)) {
+        alertMonth.textContent = "month must be valid month"
+        setTimeout(() => {
+            alertDay.textContent = "";
+        }, 4000);
+    }
+    else if (yearValue > now.getFullYear()) {
+        alertYear.textContent = "year must be in the past"
+        setTimeout(() => {
+            alertDay.textContent = "";
+        }, 4000);
+    }
+    else if (isNaN(yearValue)) {
+        alertYear.textContent = "year must be valid"
+        setTimeout(() => {
+            alertDay.textContent = "";
+        }, 4000);
     }
     else {
-        const now = new Date()
-        const birthday = new Date(yearValue, monthValue - 1, dayValue)
         let ageinmili = now.getTime() - birthday.getTime()
         let ageInYears = Math.floor(ageinmili / (1000 * 60 * 60 * 24 * 365));
         let ageInMonths = Math.floor(ageinmili / (1000 * 60 * 60 * 24 * 30));
@@ -25,26 +45,9 @@ function hundleClick() {
         console.log(ageInYears)
         console.log(ageInMonths % 12)
         console.log(ageinmili % 30)
-        if (true) {
-            spanDay.textContent = ageInDays % 30
-            spanMonth.textContent = ageInMonths % 12
-            spanYear.textContent = ageInYears
-
-        } else {
-            alertDay.textContent = "day must be valid day"
-            alertMonth.textContent = "month must be valid month"
-            alertYear.textContent = "year must be in the past"
-        }
+        spanDay.textContent = ageInDays % 30
+        spanMonth.textContent = ageInMonths % 12
+        spanYear.textContent = ageInYears
     }
 
-
-}
-
-// return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day
-function isLeapYear(year) {
-    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
-        return true;
-    } else {
-        return false;
-    }
 }
